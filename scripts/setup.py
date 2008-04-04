8f86b358791cf48549b4951f7fa405d2b1883183
Uli Fouquet <uli@gnufix.de>
2008-04-04 01:09:37 +0000

    Add scripts for preparing the environment.

--- scripts/setup.py
+++ scripts/setup.py
@@ -3,9 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name='psjscripts',
     install_requires=[],
-    py_modules = ['oooctl',],
+    py_modules = ['oooctl','prepare',],
     entry_points="""
     [console_scripts]
     oooctl = oooctl:main
+    oooprepare = prepare:prepare
+    ooorestore = prepare:restore
+    
     """
     )
