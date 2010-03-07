00559915960f4c6f61ade623a942363756b8e671
Uli Fouquet <uli@gnufix.de>
2010-03-07 13:06:48 +0000

    Emit a warning when a transform cannot be reregistered.

--- src/psj/policy/setuphandlers.py
+++ src/psj/policy/setuphandlers.py
@@ -58,8 +58,15 @@ def registerTransform(site, out, name, module):
         print >> out, "Registered transform", name
     except:
         print >> out, "Transform %s already registered. Try reregister." % name
-        transforms.unregisterTransform(name)
-        transforms.manage_addTransform(name, module)
+        try:
+            transforms.unregisterTransform(name)
+            transforms.manage_addTransform(name, module)
+        except KeyError:
+            print "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+            print "WARNING!"
+            print "The %s transform could not be installed!" % name
+            print "Try registering it manually!"
+            print "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
         pass
 
 
