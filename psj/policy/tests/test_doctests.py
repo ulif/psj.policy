54cc0137e0c9287b2f0cf55acb6fed8910bb2627
ulif <uli@gnufix.fake>
2013-08-08 11:01:11 +0200

    Use new approach to init tests, make sure profiles are executed on test startups.

--- psj/policy/tests/test_doctests.py
+++ psj/policy/tests/test_doctests.py
@@ -16,7 +16,62 @@ ptc.setupPloneSite()
 
 import psj.policy
 
-class FunctionalTestCase(ptc.FunctionalTestCase):
+
+
+from plone.app.testing import PloneSandboxLayer
+from plone.app.testing import PLONE_FIXTURE
+from plone.app.testing import IntegrationTesting, FunctionalTesting
+
+from plone.testing import z2
+
+class PSJPolicyLayer(PloneSandboxLayer):
+
+    defaultBases = (PLONE_FIXTURE,)
+
+    def setUpZope(self, app, configurationContext):
+        # Load ZCML
+        import psj.policy
+        self.loadZCML(package=psj.policy)
+
+        # Install product and call its initialize() function
+        z2.installProduct(app, 'psj.policy')
+
+        # Note: you can skip this if my.product is not a Zope 2-style
+        # product, i.e. it is not in the Products.* namespace and it
+        # does not have a <five:registerPackage /> directive in its
+        # configure.zcml.
+
+    def setUpPloneSite(self, portal):
+        # Install into Plone site using portal_setup
+        self.applyProfile(portal, 'psj.policy:default')
+
+    def tearDownZope(self, app):
+        # Uninstall product
+        z2.uninstallProduct(app, 'psj.policy')
+
+        # Note: Again, you can skip this if my.product is not a Zope 2-
+        # style product
+
+MY_PRODUCT_FIXTURE = PSJPolicyLayer()
+MY_PRODUCT_INTEGRATION_TESTING = IntegrationTesting(
+    bases=(MY_PRODUCT_FIXTURE,), name="PSJPolicy:Integration")
+MY_PRODUCT_FUNCTIONAL_TESTING = FunctionalTesting(
+    bases=(MY_PRODUCT_FIXTURE,), name="PSJPolicy:Functional")
+
+class IntegrationTestCase(unittest.TestCase):
+
+    layer = MY_PRODUCT_INTEGRATION_TESTING
+
+class FunctionalTestCase(unittest.TestCase):
+
+    layer = MY_PRODUCT_FUNCTIONAL_TESTING
+
+    @property
+    def portal(self):
+        return self.layer['portal']
+
+
+class DISFunctionalTestCase(ptc.FunctionalTestCase):
 
     class layer(PloneSite):
 
