201ae4696e9e6cfacf30b9f5951bdb1dc76c54d5
ulif <uli@gnufix.fake>
2013-09-05 06:04:18 +0200

    Add a more basic test for cmd_oooconv module.

---
+++ psj/policy/transforms/tests/test_cmd_oooconv.py
@@ -0,0 +1,34 @@
+# tests for cmd_oooconv module
+import os
+import shutil
+import tempfile
+import unittest
+from psj.policy.transforms.cmd_oooconv import Document
+from Products.PortalTransforms.data import datastream
+
+class DocumentTests(unittest.TestCase):
+
+    def setUp(self):
+        input_dir = os.path.join(os.path.dirname(__file__), 'input')
+        self.doc_simple1_path = os.path.join(input_dir, 'simpledoc1.doc')
+        self.doc_simple1 = open(self.doc_simple1_path, 'rb').read()
+        self.idata = datastream('mytestdoc.doc')
+        self.idata.setData(self.doc_simple1)
+        self.doc = None   # to be set by tests
+
+    def tearDown(self):
+        pass
+
+    def test_attribs(self):
+        # Documents have some attributes, notably a tmpdir and a fullpath
+        self.doc = Document('mytestdoc.doc', self.doc_simple1)
+        assert self.doc.tmpdir is not None
+        assert self.doc.fullname[-14:] == '/mytestdoc.doc'
+
+    def test_del_removes_tmp_dir(self):
+        # Deleted `Document`s do not leave any temp dirs
+        self.doc = Document('mytestdoc.doc', self.doc_simple1)
+        path = self.doc.fullname
+        assert os.path.isfile(path)
+        del self.doc
+        assert not os.path.isfile(path)
