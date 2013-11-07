1a416350968c021f7b2f9d41c54c23d643ff3b4a
ulif <uli@gnufix.fake>
2013-11-07 18:15:08 +0100

    Add test helper to get a modified options dict.

--- psj/policy/transforms/tests/test_processors.py
+++ psj/policy/transforms/tests/test_processors.py
@@ -3,8 +3,11 @@ import os
 import shutil
 import tempfile
 import unittest
+from ulif.openoffice.client import Client
 from ulif.openoffice.helpers import get_entry_points
+from psj.policy.transforms.cmd_oooconv import OPTIONS_HTML
 from psj.policy.transforms.processors import PSJHTMLProcessor
+
 
 
 class PSJHTMLProcessorTests(unittest.TestCase):
@@ -12,10 +15,23 @@ class PSJHTMLProcessorTests(unittest.TestCase):
     def setUp(self):
         self.workdir = tempfile.mkdtemp()
         self.in_path = os.path.join(self.workdir, 'sample.html')
+        self.css_path = os.path.join(self.workdir, 'style.css')
         open(self.in_path, 'w').write('<html><body>Hi there!<body></html>')
 
     def tearDown(self):
         shutil.rmtree(self.workdir)
+
+    @property
+    def transform_options(self):
+        # OPTIONS_HTML with 'psj_html' stripped from 'meta-procord'
+        options = dict(OPTIONS_HTML)
+        options['meta-procord'] = options['meta-procord'].replace(
+            'psj_html', '')
+        return options
+
+    def create_source(self):
+        # create an additional CSS file for use with the in_path HTML
+        open(self.css_path, 'w').write('p: { \n  text-color: #f00; }')
 
     def test_registered(self):
         # make sure the processor is registered on startup
