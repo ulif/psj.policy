a3a9779831d1f357a0c984daae4ac44ce4e050f2
ulif <uli@gnufix.de>
2015-09-08 20:02:06 +0200

    Set supported Py versions in setup.py.

--- setup.py
+++ setup.py
@@ -36,6 +36,9 @@ setup(
                  'Intended Audience :: Developers',
                  'License :: OSI Approved :: Zope Public License',
                  'Programming Language :: Python',
+                 'Programming Language :: Python :: 2 :: Only',
+                 'Programming Language :: Python :: 2.6',
+                 'Programming Language :: Python :: 2.7',
                  'Operating System :: OS Independent',
                  'Framework :: Plone',
                  (
