11b6a20073b3704dda2a5dc7cbcc10c78a03f52c
ulif <uli@gnufix.de>
2015-09-12 17:51:06 +0200

    Prepare v1.2 release.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ tests_require = [
 
 setup(
     name='psj.policy',
-    version='1.2.dev0',
+    version='1.2',
     author='Uli Fouquet',
     author_email='uli@gnufix.de',
     url='http://pypi.python.org/pypi/psj.policy',
